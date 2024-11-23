cf53bdf36383730540091a66b7a01572f1def8cd
azzimlii <ezimliinci@gmail.com>
2024-11-23 19:51:59 +0400

    second commit

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,12 +4,7 @@ import UserLogin from "./components/UserLogin";
 class App extends Component{
     render(){
         return(
-            <>
-            {/* <Wine title="A wine" description="Russian" rating="5"/>
-            <Wine title="B wine" description="American" rating="11"/>
-            <Wine title="C wine" description="Italian" rating="23"/> */
-            <UserLogin/>}
-            </>
+            <UserLogin/>
         )
     }
 }
